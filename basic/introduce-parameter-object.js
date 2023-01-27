c1268af701cdc094593104299906c426dffcc455
doyu <ydh0905@nate.com>
2023-01-27 15:00:44 +0900

    introduce parameter object after refactoring

--- basic/introduce-parameter-object.js
+++ basic/introduce-parameter-object.js
@@ -1,5 +1,26 @@
-function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+function readingsOutsideRange(readings, range) {
+  return readings.filter((r) => !range.contains(r.temp));
+}
+
+class NumberRange {
+  #min;
+  #max;
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+
+  get min() {
+    return this.#min;
+  }
+
+  get max() {
+    return this.#max;
+  }
+
+  contains(number) {
+    return number >= this.#min && number <= this.#max;
+  }
 }
 
 const station = {
@@ -12,13 +33,9 @@ const station = {
     { temp: 51, time: '2016-11-10 09:50' },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+const operationPlan = new NumberRange(51, 53);
+
+// station.readings 값만 필요로 하기 때문에 station.readings를 전달해준다.
+// min, max를 따로 풀어서 전달하는 것보다 객체 자체를 전달해준다.
+readingsOutsideRange(station.readings, operationPlan);
